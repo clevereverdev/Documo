56b5668f5ad5f14dbe17c292f2630957b41ca680
Amritvir Randhawa <arandhawa@mail.csuchico.edu>
2023-10-06 17:38:04 -0700

    Fixed some bugs and issues for Sidebar.

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
-import side_navbar from "components/Sidebar";
+import React from "react";
+
 import { AuthUserProvider } from "../firebase/auth";
 import '../styles/globals.css';
 import Head from "next/head";
 import { ToastContainer } from 'react-toastify';
 
-
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Docomo</title>
-      </Head>
-      <AuthUserProvider>
-      <side_navbar/>
-      <ToastContainer/>
-        <Component {...pageProps} />
-      </AuthUserProvider>
-    </>
-  );
-}
+    return (
+      <>
+        <Head>
+          <title>Docomo</title>
+        </Head>
+        <AuthUserProvider>
+        <ToastContainer/>
+          <Component {...pageProps} />
+        </AuthUserProvider>
+      </>
+    );
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import styles from "../styles/Home.module.css";
-import { GoSignOut } from "react-icons/go";
 import { useState, useEffect } from "react";
-import { useAuth  } from "../firebase/auth";
+import { useAuth } from "../firebase/auth";
 import { useRouter } from "next/router";
 import Loader from "../components/loader";
-import SideNavbar from "components/Sidebar";
+import Layout from "@/Sidebar";
 
 export default function Home() {
-    const { authUser, isLoading, signOut } = useAuth();
-    const [showSideNavbar, setShowSideNavbar] = useState(false); 
+    const { authUser, isLoading } = useAuth();
+    const [showSideNavbar, setShowSideNavbar] = useState(false);
     const router = useRouter();
 
     useEffect(() => {
@@ -19,26 +18,19 @@ export default function Home() {
         }
     }, [authUser, isLoading, router]);
 
-    
-
     return !authUser ? (
         <Loader />
     ) : (
-
-        <div className={styles.container}>
-            {showSideNavbar && <SideNavbar/>}
-            <div className="flex items-center justify-between gap-2 font-medium shadow-md fixed top-7 right-60">
-                <h1>
-                    Welcome,{" "}
-                    <strong className="text-green-600">{authUser.username}</strong>!
-                </h1>
-                <div
-                    className="bg-black text-white w-44 py-4 rounded-lg transition-transform hover:bg-black/[0.8] active:scale-90 flex items-center justify-center gap-2 font-medium shadow-md fixed top-2 right-5 cursor-pointer"
-                    onClick={signOut}>
-                    <GoSignOut size={18} />
-                    <span>Logout</span>
+        <Layout>
+            <div className={styles.container}>
+                <div className="flex items-center justify-between gap-2 font-medium shadow-md fixed top-7 right-60">
+                    <h1>
+                        Welcome,{" "}
+                        <strong className="text-green-600">{authUser.username}</strong>!
+                    </h1>
                 </div>
             </div>
-        </div>
+        </Layout>
     );
 }
+
